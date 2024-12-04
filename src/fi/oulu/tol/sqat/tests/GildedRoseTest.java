496cb54394a737b299dd01ffe763a02b25fb9293
f1sheater <hsulin22@student.oulu.fi>
2024-12-04 23:45:25 +0200

    TASK-2

--- src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
+++ src/fi/oulu/tol/sqat/tests/GildedRoseTest.java
@@ -29,4 +29,131 @@ public class GildedRoseTest {
 		//assert quality has decreased by one
 		assertEquals("Failed quality for Dexterity Vest", 19, quality);
 	}
+	
+	@Test
+	public void test_sellIn() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Dagger", 5, 5));
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int sellIn = items.get(0).getSellIn();
+
+		assertEquals("Failed sellIn for Dagger", 4, sellIn);
+	}
+	
+	@Test
+	public void test_qualityAfterExpiration() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Dagger", 1, 30));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+
+		assertEquals("Failed quality for Dagger", 25, quality);
+	}
+	
+	@Test
+	public void test_qualityNotNegative() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Dagger", 1, 1));
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+
+		assertEquals("Failed quality for Dagger", 0, quality);
+	}
+	
+	@Test
+	public void test_BrieAging() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Aged Brie", 3, 10));
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals("Failed quality for Brie", 12, quality);
+	}
+	
+	@Test
+	public void test_BrieAgingOver50() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Aged Brie", 1, 47));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals("Failed quality for Brie", 50, quality);
+	}
+	
+	@Test
+	public void test_BackstageAging() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 11, 1));
+		inn.oneDay(); // 2
+		inn.oneDay(); // 4
+		inn.oneDay(); // 6
+		inn.oneDay(); // 8
+		inn.oneDay(); // 10
+		inn.oneDay(); // 12
+		inn.oneDay(); // 15
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals("Failed quality for Backstage pass", 15, quality);
+	}
+	
+	@Test
+	public void test_BackstageQualityAtZero() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Backstage passes to a TAFKAL80ETC concert", 2, 10));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals("Failed quality for Backstage pass", 0, quality);
+	}
+	
+	@Test
+	public void test_SulfurasQuality() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", 4, 80));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int quality = items.get(0).getQuality();
+		
+		assertEquals("Failed quality for Sulfuras", 80, quality);
+	}
+	
+	@Test
+	public void test_SulfurasAging() {
+		GildedRose inn = new GildedRose();
+		inn.setItem(new Item("Sulfuras, Hand of Ragnaros", 4, 80));
+		inn.oneDay();
+		inn.oneDay();
+		inn.oneDay();
+		
+		List<Item> items = inn.getItems();
+		int sellIn = items.get(0).getSellIn();
+		
+		assertEquals("Failed quality for Sulfuras", 4, sellIn);
+	}
 }
